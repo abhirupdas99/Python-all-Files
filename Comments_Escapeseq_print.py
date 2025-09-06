e16688a217675980ec7309da191449dd7875a612
abhirupdas99 <abhirup99official@gmail.com>
2025-09-05 23:08:11 -0400

    pushing day2 content

---
+++ Comments_Escapeseq_print.py
@@ -0,0 +1,29 @@
+
+#for shortcut of multiple comments use Ctrl + /
+#for shortcut of multiple selection use Alt + Shift + Arrow keys
+#using triple quotes for multi-line comments
+'''
+This is a multi-line comment.
+It can span multiple lines.
+'''
+
+#using triple double quotes for multi-line comments
+"""
+This is a multi-line comment.
+It can also span multiple lines.
+""" 
+
+#printing a sentence
+print("This is a string with double quotes")
+
+#using escape sequence for new line
+print("This is the first line.\nThis is the second line.")
+
+#using escape sequence for single quote
+print('It\'s a beautiful day!')
+
+#using escape sequence to print double quotes inside a string
+print("This is a string with \"double quotes\" inside it")
+
+#using comma to print multiple items in one line
+print("Hello,", "world!", 0,0,7, "Welcome to Python programming.")
